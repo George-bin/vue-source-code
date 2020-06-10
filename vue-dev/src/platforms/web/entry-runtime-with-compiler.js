fbc967d0447022047b30badfea1d80b99d5fd3d4
gsb <770021714@qq.com>
2020-06-10 23:06:31 +0800

    。

--- vue-dev/src/platforms/web/entry-runtime-with-compiler.js
+++ vue-dev/src/platforms/web/entry-runtime-with-compiler.js
@@ -14,17 +14,22 @@ const idToTemplate = cached(id => {
   return el && el.innerHTML
 })
 
-// 缓存Vue.prototype.$mount
+// 缓存Vue.prototype.$mount => 这里对应runtime/index.js中的$mount方法
 const mount = Vue.prototype.$mount
-// runtime-compiler相关逻辑，runtime-only并不需要
+/**
+ * 编译生成render函数（Runtime+Compiler版本的相关逻辑，Runtime Only版本并不需要）
+ * @params el：要挂载的元素的id选择器，例如：#app
+ * @params hydrating：和服务端渲染相关，浏览器环境下不需要传第二个参数
+ */
 Vue.prototype.$mount = function (
   el?: string | Element,
   hydrating?: boolean
 ): Component {
+  // 根据id选择器获取真实DOM元素
   el = el && query(el)
 
   /* istanbul ignore if */
-  // 对el参数做了一次处理，vue实例不允许挂载到html或body上
+  // 做了一下限制，vue实例不允许挂载到html或body上
   if (el === document.body || el === document.documentElement) {
     process.env.NODE_ENV !== 'production' && warn(
       `Do not mount Vue to <html> or <body> - mount to normal elements instead.`
@@ -34,9 +39,9 @@ Vue.prototype.$mount = function (
 
   const options = this.$options
   // resolve template/el and convert to render function
-  if (!options.render) { // 判断是否存在render函数
+  if (!options.render) { // 不存在render函数
     let template = options.template
-    if (template) { // 判断是否存在template
+    if (template) { // 存在template
       if (typeof template === 'string') {
         if (template.charAt(0) === '#') {
           template = idToTemplate(template)
